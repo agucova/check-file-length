d168d79e7527e6b9d4e6ed037fa837923078d77f
Agustín Covarrubias <agucova@uc.cl>
2021-08-19 01:20:37 -0400

    fix: adapt to one file

--- check_long_files.bash
+++ check_long_files.bash
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-returnCode=0
-for i in $(find . -name "*.py" -type f); do
-	 length="$(wc -l < "$i" | xargs)"
-	 if [[ "$length" -gt "400" ]] ; then
-	 	echo "$i: $length"
-	 	returnCode=1
-	 fi
-done
-
-exit $returnCode
+length="$(wc -l < "$1" | xargs)"
+if [[ "$length" -gt "400" ]] ; then
+	echo "$i: $length"
+	exit 1
+fi
