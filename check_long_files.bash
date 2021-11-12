c25be640dc3c7bd755218b76bdcdb639a2f585cd
Agustín Covarrubias <agucova@uc.cl>
2021-11-12 19:02:11 -0300

    Update check_long_files.bash

--- check_long_files.bash
+++ check_long_files.bash
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 returnCode=0
 for i in $(git ls-files | grep -E "\.py$" | xargs -d '\n' ls -1df 2>/dev/null); do
-	 echo "Checking $i"
 	 lines="$(wc -l < "$i" | xargs)"
-     maxlength="$(wc -L < "$i" | xargs)"
+	 maxlength="$(wc -L < "$i" | xargs)"
 	 if [[ "$lines" -gt "400" || "$maxlength" -gt "100" ]] ; then
 	 	echo "$i: $lines lines, $maxlength max_length"
 	 	returnCode=1
